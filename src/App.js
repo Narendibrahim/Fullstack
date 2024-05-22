1e6ab87fa5d2d0a32e952d9a734ef1ef4de8d622
Narendibrahim <162007956+Narendibrahim@users.noreply.github.com>
2024-05-23 00:05:44 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Component/Header';
+import GoalList from './Component/GoalList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, I'm - Narend!</h1>
-        <p>This year, I'm turning 19 years old and I live in South Tangerang.</p>
-        <p>My full name is Narendra Ibrahim Nurtaqiy and I am a Digital Business student at Padjadjaran University.</p>
-      </header>
+      <Header />
+      <GoalList />
     </div>
   );
 }
